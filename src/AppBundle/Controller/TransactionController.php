6a747f81068b9d9e2e8b484895ea65d4feb7c557
César Rodríguez <kesarr@gmail.com>
2018-02-24 14:07:48 +0100

    Revert "Issue 8 trans timestamp"

--- src/AppBundle/Controller/TransactionController.php
+++ src/AppBundle/Controller/TransactionController.php
@@ -18,7 +18,7 @@ class TransactionController extends Controller
     public function transactionsAction(Request $request)
     {
         $size = (int)$request->get('size', self::DEFAULT_SIZE);
-        $filter = (object) [];
+        $filter = [];
         if ($request->get('transaction_id')) {
             $filter = ['transaction_id' => (string)$request->get('transaction_id')];
         }
@@ -31,28 +31,11 @@ class TransactionController extends Controller
         $items = [];
         $db = $this->get('eos_explorer.mongo_service');
 
-        $cursor = $db->get()->Transactions->aggregateCursor( [
-            [ '$match' => $filter ],
-            [ '$lookup' => [
-                     'from' => 'Blocks',
-                     'localField' => 'block_id',
-                     'foreignField' => 'block_id',
-                     'as' => 'block_docs' ] ], 
-            [ '$unwind' => '$block_docs' ], 
-            [ '$project' => [
-                     'ref_block_timestamp' => '$block_docs.timestamp',
-                     'transaction_id' => 1,
-                     'sequence_num' => 1,
-                     'block_id' => 1,
-                     'ref_block_num' => 1,
-                     'ref_block_prefix' => 1,
-                     'expiration' =>  1,
-                     'signatures' => 1,
-                     'actions' => 1 ] ],
-            [ '$sort' => [ 'ref_block_timestamp' => -1 ] ],
-            [ '$skip' => (int)$request->get('page', 0) * $size ],
-            [ '$limit' => $size ]
-        ] );
+        $cursor = $db->get()->Transactions
+            ->find($filter)
+            ->sort(['createdAt' => -1])
+            ->skip((int)$request->get('page', 0) * $size)
+            ->limit($size);
 
         foreach ($cursor as $key => $document) {
             $items[] = $document;
