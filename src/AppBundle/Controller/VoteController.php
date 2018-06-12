f5733fe299a10cfe7871b4215b24e11116cb37c4
Cesar Rodriguez <kesarr@gmail.com>
2018-06-12 20:22:31 +0200

    votes

---
+++ src/AppBundle/Controller/VoteController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VoteController extends Controller
+{
+    /**
+     * @Route("/votes/{producer}", name="votes")
+     */
+    public function votesAction(string $producer, Request $request)
+    {
+        $cache = $this->get('api.cache_service');
+        $service = $this->get('api.vote_service');
+        $page = $request->query->getInt('page', 0);
+        $items = $cache->get()->get('votes_'.$producer.$page);
+        if (!$items) {
+            $items = $service->forProducer($producer, $page);
+            $cache->get()->set('producers.action', $items, 60);
+        }
+
+        return new JsonResponse($items);
+    }
+}
