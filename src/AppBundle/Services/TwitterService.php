b11e95a6d59541ebe8d2bc2549c06ab5e6b95f45
Cesar Rodriguez <kesarr@gmail.com>
2018-06-15 16:17:04 +0200

    api

--- src/AppBundle/Services/TwitterService.php
+++ src/AppBundle/Services/TwitterService.php
@@ -2,14 +2,22 @@
 
 namespace AppBundle\Services;
 
-use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\EntityManager;
 
-class TwitterService extends EntityRepository
+class TwitterService
 {
+
+    private $entityManager;
+
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public function all(int $page = 0): ?array
     {
         $sql = "SELECT created_at, aa.actor, JSON_UNQUOTE(data->\"$.msg\") as msg FROM actions a JOIN actions_accounts aa ON a.id = aa.action_id JOIN transactions t ON a.transaction_id = t.id WHERE account = 'decentwitter' LIMIT 50 OFFSET ".$page * 50;
-        $stmt = $this->getEntityManager()->getConnection()->prepare($sql);
+        $stmt = $this->entityManager->getConnection()->prepare($sql);
         $stmt->execute();
         return $stmt->fetchAll();
     }
@@ -17,7 +25,7 @@ class TwitterService extends EntityRepository
     public function forUser(string $account, int $page = 0): ?array
     {
         $sql = "SELECT created_at, aa.actor, JSON_UNQUOTE(data->\"$.msg\") as msg FROM actions a JOIN actions_accounts aa ON a.id = aa.action_id JOIN transactions t ON a.transaction_id = t.id WHERE account = 'decentwitter' AND aa.actor='".$account."' LIMIT 50 OFFSET ".$page * 50;
-        $stmt = $this->getEntityManager()->getConnection()->prepare($sql);
+        $stmt = $this->entityManager->getConnection()->prepare($sql);
         $stmt->execute();
         return $stmt->fetchAll();
     }
