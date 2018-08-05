61ecb909f740a79faac061489a40992950c11e10
Cesar Rodriguez <kesarr@gmail.com>
2018-08-05 11:47:23 +0200

    🚀

--- src/AppBundle/Controller/ActionController.php
+++ src/AppBundle/Controller/ActionController.php
@@ -57,14 +57,15 @@ class ActionController extends Controller
     /**
      * @Route("/transactions/{tx}/actions/{seq}", name="tx_action_seq")
      */
-    public function actionSeqAction(string $tx, string $seq)
+    public function actionSeqAction(string $tx, string $seq, Request $request)
     {
-        $result = $this->get('cache.app')->getItem('tx_action_'.$tx.'_'.$seq);
+        $parent = $request->request->getInt('parentId', 0);
+        $result = $this->get('cache.app')->getItem('tx_action_'.$tx.'_'.$seq.'_'.$parent);
         if (!$result->isHit()) {
             $serviceTx = $this->get('api.transaction_service');
             $transaction = $serviceTx->findOneBy(['id' => $tx]);
             $service = $this->get('api.action_service');
-            $item = $service->findOneBy(['transaction' => $transaction, 'seq' => $seq]);
+            $item = $service->findOneBy(['transaction' => $transaction, 'seq' => $seq, 'parent_id' => $parent]);
             if (!$item) {
                 return new JsonResponse(['error' => 'entity not found'], 404);
             }
